84baa65e83cae7e81f28559673fcb608e72f679e
Joshua Mari P. Coquia <coquia-kdt@kdts.net>
2023-12-19 16:10:21 +0800

    no data colspan fix

--- checkAvailability/js/individual.js
+++ checkAvailability/js/individual.js
@@ -402,7 +402,7 @@ function fillHistory(dlist) {
   tableBody.empty();
   if (dlist.length === 0) {
     var noDataRow = $(
-      "<tr><td colspan='6' class='text-center'>No data found</td></tr>"
+      "<tr><td colspan='7' class='text-center'>No data found</td></tr>"
     );
     tableBody.append(noDataRow);
   } else {
