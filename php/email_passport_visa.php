1cfbeaaae373426867f65c016a6532a9282528e3
Dexmel <dexmel23@gmail.com>
2023-12-22 15:17:17 +0800

    update email send

--- php/email_passport_visa.php
+++ php/email_passport_visa.php
@@ -9,37 +9,33 @@ date_default_timezone_set('Asia/Manila');
 #endregion
 
 #region Initialize variables
-$sendDays = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270];
+
 #endregion
 
 #region main function
 try {
     $expiryPassQ = "SELECT ed.emp_email as empEmail, TIMESTAMPDIFF(DAY, CURDATE(), pd.passport_expiry) AS expiringIn, pd.passport_expiry as expiringDate, pd.passport_number as 
-    passportNum FROM employee_details as ed LEFT JOIN passport_details as pd ON ed.emp_number = pd.emp_number WHERE pd.passport_expiry >= CURDATE() AND pd.passport_expiry <= 
+    idNum FROM employee_details as ed LEFT JOIN passport_details as pd ON ed.emp_number = pd.emp_number WHERE pd.passport_expiry >= CURDATE() AND pd.passport_expiry <= 
     DATE_ADD(CURDATE(), INTERVAL 9 MONTH)";
     $expiryPassStmt = $connpcs->prepare($expiryPassQ);
     $expiryPassStmt->execute([]);
-    $expiryPass = $expiryPassStmt->fetchAll();
+    if ($expiryPassStmt->rowCount() > 0) {
+        $type = "passport";
+        $expiryPass = $expiryPassStmt->fetchAll();
+        $sendDays = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270];
+        sendEmail($expiryPass, $sendDays, $type);
+    }
 
-    foreach ($expiryPass as $val) {
-        $expiringIn = $val["expiringIn"];
-        $expiringDate = $val["expiringDate"];
-        $email = $val["empEmail"];
-        $passportNum = $val["passportNum"];
-
-        if (in_array($expiringIn, $sendDays)) {
-            $expiringDate = strtotime($expiringDate);
-            $expiringDate = date("d M Y", $expiringDate);
-            $message = "Your passport with number " . $passportNum . " will be expiring in " . $expiringDate;
-            if ($expiringIn == 0) {
-                $message = "Your passport with number " . $passportNum . " will be expiring today";
-            }
-
-            sendEmail($email, $expiringDate, $message);
-            echo "success " . $expiringIn . " " . $email;
-        } else {
-            echo "fail " . $expiringIn . " " . $email;
-        }
+    $expiryVisaQ = "SELECT ed.emp_email as empEmail, TIMESTAMPDIFF(DAY, CURDATE(), vd.visa_expiry) AS expiringIn, vd.visa_expiry as expiringDate, vd.visa_number as 
+    idNum FROM employee_details as ed LEFT JOIN visa_details as vd ON ed.emp_number = vd.emp_number WHERE vd.visa_expiry >= CURDATE() AND vd.visa_expiry <= 
+    DATE_ADD(CURDATE(), INTERVAL 9 MONTH)";
+    $expiryVisaStmt = $connpcs->prepare($expiryVisaQ);
+    $expiryVisaStmt->execute([]);
+    if ($expiryVisaStmt->rowCount() > 0) {
+        $type = "visa";
+        $expiryVisa = $expiryVisaStmt->fetchAll();
+        $sendDays = [0, 30, 60, 90, 120, 150, 180];
+        sendEmail($expiryVisa, $sendDays, $type);
     }
 } catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
@@ -47,12 +43,31 @@ try {
 #endregion
 
 
-function sendEmail($email, $expiringDate, $message)
+function sendEmail($expiryID, $sendDays, $type)
 {
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     $headers .= "From: kdtexpiringpassportreminder@corp.khi.co.jp";
     $subject = 'Expiring Passport';
 
-    mail($email, $subject, $message, $headers);
+    foreach ($expiryID as $val) {
+        $expiringIn = $val["expiringIn"];
+        $expiringDate = $val["expiringDate"];
+        $email = $val["empEmail"];
+        $idNum = $val["idNum"];
+
+        if (in_array($expiringIn, $sendDays)) {
+            $expiringDate = strtotime($expiringDate);
+            $expiringDate = date("d M Y", $expiringDate);
+            $message = "Your " . $type . " with number " . $idNum . " will be expiring in " . $expiringDate . " (for testing)";
+            if ($expiringIn == 0) {
+                $message = "Your " . $type . " with number " . $idNum . " will be expiring today (for testing)";
+            }
+
+            mail($email, $subject, $message, $headers);
+            echo "success " . $expiringIn . " " . $email;
+        } else {
+            echo "fail " . $expiringIn . " " . $email;
+        }
+    }
 }
