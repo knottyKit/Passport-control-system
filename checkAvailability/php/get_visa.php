6d21b79cc996d517885032041da8df5882e0e121
Dexmel <hernandez-kdt@global.kasaki.com>
2024-01-17 13:44:45 +0800

    checkavailability passport and visa dates format

--- checkAvailability/php/get_visa.php
+++ checkAvailability/php/get_visa.php
@@ -8,80 +8,43 @@ date_default_timezone_set('Asia/Manila');
 #endregion
 
 #region Initialize Variable
+$empID = 0;
+$visaDeets = array();
+#endregion
 
-$visa = array();
-$empID = NULL;
-if (!empty($_POST['empID'])) {
-    $empID = (int)$_POST['empID'];
-} else {
-    die(json_encode($visa));
-}
-$startDate = NULL;
-if (!empty($_POST['sDate'])) {
-    $startDate = $_POST['sDate'];
-}
-$endDate = NULL;
-if (!empty($_POST['eDate'])) {
-    $endDate = $_POST['eDate'];
+#region get data values
+if (!empty($_POST["empID"])) {
+    $empID = $_POST["empID"];
 }
 #endregion
 
-#region main query
+#region main function
 try {
-    $visaQ = "SELECT visa_number,visa_issue,visa_expiry  FROM `visa_details` WHERE emp_number = :empID";
+    $visaQ = "SELECT visa_number as number, visa_issue as issue, visa_expiry as expiry FROM visa_details WHERE emp_number = :empID";
     $visaStmt = $connpcs->prepare($visaQ);
-    $visaStmt->execute([":empID" => $empID]);
+    $visaStmt->execute([":empID" => "$empID"]);
     if ($visaStmt->rowCount() > 0) {
-        $visaarr = $visaStmt->fetchAll();
-        foreach ($visaarr as $vs) {
-            $number = $vs['visa_number'];
-            $issue = $vs['visa_issue'];
-            $expiry = $vs['visa_expiry'];
-            $validity_dates = [
-                'issue' => $issue,
-                'expiry' => $expiry,
-            ];
-            $check_dates = [
-                'start' => $startDate,
-                'end' => $endDate,
-            ];
-            $isValid = isValid($validity_dates, $check_dates);
-            $visa += ["number" => $number];
-            $visa += ["issue" => $issue];
-            $visa += ["expiry" => $expiry];
-            $visa += ["valid" => $isValid];
+        $visaDeets = $visaStmt->fetch();
+        $isValid = true;
+        $expiry = new DateTime($visaDeets["expiry"]);
+        $dateNow = new DateTime();
+        if ($expiry < $dateNow) {
+            $isValid = false;
         }
+
+        if ($visaDeets["issue"] !== null) {
+            $visaIs = strtotime($visaDeets["issue"]);
+            $visaDeets["issue"] = date("d M Y", $visaIs);
+        }
+        if ($visaDeets["expiry"] !== null) {
+            $visaEx = strtotime($visaDeets["expiry"]);
+            $visaDeets["expiry"] = date("d M Y", $visaEx);
+        }
+        $visaDeets['valid'] = $isValid;
     }
 } catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-
 #endregion
 
-#region FUNCTIONS
-function isValid($validity, $chkRange)
-{
-    $issue_date_timestamp = strtotime($validity['issue']);
-    $expiry_date_timestamp = strtotime($validity['expiry']);
-    $start_date = $chkRange['start'];
-    $end_date = $chkRange['end'];
-
-    // Both start and end dates are null, consider it valid
-    if ($start_date === null && $end_date === null) {
-        return true;
-    }
-
-    // Check if start date is provided and is after the visa issue date
-    if ($start_date !== null && strtotime($start_date) < $issue_date_timestamp) {
-        return false;
-    }
-
-    // Check if end date is provided and is before the visa expiry date
-    if ($end_date !== null && strtotime($end_date) > $expiry_date_timestamp) {
-        return false;
-    }
-
-    return true;
-}
-#endregion
-echo json_encode($visa);
+echo json_encode($visaDeets);
