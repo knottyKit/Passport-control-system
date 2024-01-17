be10b2a471cb329ffbbb2e5e2ae5ba59f90bbfb0
Dexmel <hernandez-kdt@global.kasaki.com>
2024-01-17 09:17:42 +0800

    update

--- report/js/individual.js
+++ report/js/individual.js
@@ -152,7 +152,6 @@ function createTable(repData) {
 function getReport() {
   const grpID = $("#grpSel").find("option:selected").attr("grp-id");
   const yr = $("#yearSel").val();
-  console.log(yr, " getReport");
   return new Promise((resolve, reject) => {
     $.ajax({
       type: "POST",
